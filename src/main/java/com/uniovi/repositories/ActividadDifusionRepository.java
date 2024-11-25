ede202faf175b6c8204a39679e4f8ced4efb5e0b
Anastasiia <anstasiakim2789@gmail.com>
2024-11-25 12:08:24 +0100

    craeted ActividadDifusionRepository

---
+++ src/main/java/com/uniovi/repositories/ActividadDifusionRepository.java
@@ -0,0 +1,8 @@
+package com.uniovi.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.uniovi.entities.ActividadDifusion;
+
+public interface ActividadDifusionRepository extends JpaRepository<ActividadDifusion, Integer> {
+}
