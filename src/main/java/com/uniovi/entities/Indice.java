38852f7075c53f88435c7d24db0f75d3024269ea
Anastasiia <anstasiakim2789@gmail.com>
2025-01-23 13:41:03 +0100

    created entity Indice

---
+++ src/main/java/com/uniovi/entities/Indice.java
@@ -0,0 +1,55 @@
+package com.uniovi.entities;
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Indices")
+public class Indice {
+	
+	    @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Integer id;
+	 
+	    @Column(nullable = false, length = 255)
+	    private String nombre;
+
+	    @Column(nullable = false)
+	    private LocalDate fecha_de_inicio;
+	    
+	    @Column(nullable = false)
+	    private LocalDate fecha_de_fin;
+
+		public String getNombre() {
+			return nombre;
+		}
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+		public Indice(Integer id, String nombre, LocalDate fecha_de_inicio, LocalDate fecha_de_fin) {
+			super();
+			this.id = id;
+			this.nombre = nombre;
+			this.fecha_de_inicio = fecha_de_inicio;
+			this.fecha_de_fin = fecha_de_fin;
+		}
+
+		public LocalDate getFecha_de_inicio() {
+			return fecha_de_inicio;
+		}
+
+		public void setFecha_de_inicio(LocalDate fecha_de_inicio) {
+			this.fecha_de_inicio = fecha_de_inicio;
+		}
+
+		public LocalDate getFecha_de_fin() {
+			return fecha_de_fin;
+		}
+
+		public void setFecha_de_fin(LocalDate fecha_de_fin) {
+			this.fecha_de_fin = fecha_de_fin;
+		}
+	    
+	    
+}
