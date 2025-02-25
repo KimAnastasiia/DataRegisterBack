2e2651179aaf9ff9de40794964b7e362b0fde71b
Anastasiia <anstasiakim2789@gmail.com>
2025-02-25 13:19:34 +0100

    media de porcentajes participantes (dentro del rango de fechas) en InnovacionDocente

--- src/main/java/com/uniovi/controllers/InnovacionDocenteController.java
+++ src/main/java/com/uniovi/controllers/InnovacionDocenteController.java
@@ -1,9 +1,12 @@
 package com.uniovi.controllers;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.uniovi.entities.InnovacionDocente;
@@ -29,12 +33,18 @@ public class InnovacionDocenteController {
 	        return service.getAll();
 	    }
 	    
+	    @GetMapping("/media/average")
+	    public Double getAverage(@RequestParam String startDate, @RequestParam String endDate) {
+	         LocalDate start = LocalDate.parse(startDate);
+	         LocalDate end = LocalDate.parse(endDate);
+	         return Optional.ofNullable(service.getAverageParticipation(start, end)).orElse(0.0);
+	    }
+	    
 	    @GetMapping("/{id}")
 	    public ResponseEntity<InnovacionDocente> getById(@PathVariable Long id) {
 	        Optional<InnovacionDocente> result = service.getById(id);
 	        return result.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
 	    }
-	    
 	    @PostMapping
 	    public InnovacionDocente save(@RequestBody InnovacionDocente innovacionDocente) {
 	        return service.save(innovacionDocente);
